99fb5ef1a257308857c86efee685b62bf3791ba2
Angelo7x <abennici96@gmail.com>
2021-07-28 16:29:20 +0200

    Fine Pass Creatorr

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*chiedi nome*/
+var firstname = prompt("Inserisci il tuo nome")
+/*chiedi cognome*/
+var surname = prompt ("inserisci il tuo cognome");
+/* chiedi colore preferito*/
+var color = prompt ("Inserisci il tuo colore preferito");
+/*Chiedi la classe*/
+var classe = prompt ("Inserisci la classe");
+/*insieme dati*/
+var all = firstname + surname + color + classe;
+/*Password finale*/
+document.getElementById("pass" ) .innerHTML = all;
